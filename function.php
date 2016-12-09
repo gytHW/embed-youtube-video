d46f0ecb2db963546a6aaaf3f122db2709994fbe
Nagisao <ytgaowhu@gmail.com>
2016-12-09 18:41:05 +0800

    Update function.php

--- function.php
+++ function.php
@@ -1,7 +1,13 @@
 <?php 
 
-public function change($content)
+public function linkToEmbedVideo($content)
 {
     $resultContent = preg_replace('/\<a\shref="http:\/\/www.youtube.com\/watch\?v=([\s\S]*?)"\starget="_blank">[\s\S]*?<\/a>/i','<iframe width="560" height="315" src="https://www.youtube.com/embed/${1}" frameborder="0" allowfullscreen></iframe>' , $content);
     return $resultContent;
+}
+
+public function bbcodeToLink($content)
+{
+    $tempContent = preg_replace('/\[URL=([\s\S]*?)\]([\s\S]*?)\[[\/]URL\]/i', '<a href="${1}" target="_blank">${2}</a>', $content);
+    return $tempContent;
 }
